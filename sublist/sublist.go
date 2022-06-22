9c6c392a59eae913e965ed5285a96c2e9da3a393
Ilingu <thetitouoff@gmail.com>
2022-06-22 16:57:35 +0200

    Sublist Improvments

--- sublist/sublist.go
+++ sublist/sublist.go
@@ -9,57 +9,74 @@ const (
 	unequal   = "unequal"
 )
 
-func IsSublist(smaller, bigger []int, inversed ...bool) Relation {
+func IsEqual(l1, l2 []int) bool {
+	if len(l1) != len(l2) {
+		return false
+	}
+	// No matter whether I pick len(l2) or len(l1) since they have the same length
+	for i := 0; i < len(l2); i++ {
+		if l1[i] != l2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func IsSublist(smaller, bigger []int) bool {
+	if len(smaller) >= len(bigger) {
+		return false
+	}
+
 	SublistFound := true
+
 	var OrderedFound, LastIndexEq int
-	for i, SmVal := range smaller {
-		for j, BigVal := range bigger {
+	for i, BigVal := range bigger {
+		for j, SmVal := range smaller {
 			SublistFound = false
 
-			eq := SmVal == BigVal
-			if !eq {
+			if SmVal != BigVal {
 				continue // If no equality, we pass
 			}
 
-			if i == 0 {
+			if j == 0 {
 				OrderedFound = 1 // New Potential Sublist Detected
 			}
 
 			// If true, that means there is a no gap between two element of the sublist, they follow each other
-			if LastIndexEq+1 == j {
+			if LastIndexEq+1 == i {
 				OrderedFound++
 				if OrderedFound == len(smaller)-1 {
-					SublistFound = true
+					return true // Sublist found
 				}
 			} else {
 				OrderedFound = 0
 			}
 
-			LastIndexEq = j // Keeping track of where was the last time a equality between L1 and L2 occur
+			LastIndexEq = i // Keeping track of where was the last time a equality between L1 and L2 occur
 			break
 		}
-
-		if SublistFound {
+		// If no other natural possibility
+		if i > len(bigger)-(len(bigger)-len(smaller)) && OrderedFound == 0 {
 			break
 		}
 	}
 
-	if len(smaller) == len(bigger) && SublistFound {
-		return equal
-	}
-	if SublistFound {
-		if inversed != nil {
-			return superlist
-		}
-		return sublist
-	}
-	return unequal
+	return SublistFound
 }
 
 func Sublist(l1, l2 []int) Relation {
-	// Check Smaller, to pass the args in the right order to IsSublist
-	if len(l1) > len(l2) {
-		return IsSublist(l2, l1, true)
+	if IsEqual(l1, l2) {
+		return equal
 	}
-	return IsSublist(l1, l2)
+
+	if IsSublist(l1, l2) {
+		return sublist
+	}
+
+	// Not Sublist --> Check Superlist
+	if IsSublist(l2, l1) {
+		return superlist
+	}
+
+	return unequal // If nor equal nor sublist nor superlist, it have to be unequal
 }
